e4d1b97ec0e5d206ec47b5dfad8246efd30d26fb
gtaing <georges.taing@etu.imt-lille-douai.fr>
2021-01-03 17:25:51 +0000

    scroll view with clickable items

--- app/src/main/java/android/example/workoutapp/ListExercisesActivity.java
+++ app/src/main/java/android/example/workoutapp/ListExercisesActivity.java
@@ -1,5 +1,6 @@
 package android.example.workoutapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -10,15 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class ListExercisesActivity extends AppCompatActivity {
 
     ListView simpleList;
-    String[] countryList = {"Exercise 1", "Exercise 2", "Exercise 3", "Exercise 4", "Exercise 5", "Exercise 6", "Exercise 7", "Exercise 8", "Exercise 9"};
+    String[] exerciseList = new String[] {
+            "Exercise 1", "Exercise 2", "Exercise 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +38,55 @@ public class ListExercisesActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
-        simpleList = (ListView) findViewById(R.id.listViewExercisesID);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textViewID, countryList);
-        simpleList.setAdapter(adapter);
+        CustomAdapter customAdapter = new  CustomAdapter();
 
+        simpleList = (ListView) findViewById(R.id.listViewExercisesID);
+        simpleList.setAdapter(customAdapter);
+
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), Exercise1Activity.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        Toast.makeText(ListExercisesActivity.this, "I am the case n° " + customAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+                };
+
+            }
+        });
+    }
+    private class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            // return ListViewImages.length();
+            return 3;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.activity_list_view, null);
+            TextView name = view1.findViewById(R.id.exerciseTitleID);
+            TextView descp = view1.findViewById(R.id.exerciseDescriptionID);
+            // ImageView image = view1.findViewById(R.id.list_view_images);
+
+            name.setText(exerciseList[i]);
+            // descp.setText(exerciseDescription[i]);
+            // image.setImageResource(ListImage[i]);
+            return view1;
+        }
     }
 }
